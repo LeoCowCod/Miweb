ddb9dd15c5aa1510424ed5015cd5afd82e4e5417
cowman28 <leonardo.bc.2804@gmail.com>
2024-08-05 21:18:31 -0500

    pokeapi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Texto from "./components/Texto"
+import Contador from "./components/Contador";
+import Pokemones from "./components/Pokemones";
+
+import "./App.css"
+const App = () => {
+    return <div className="container">
+        {
+
+        }
+    <Pokemones /> 
+    </div>
+        
+}
+
+export default App;
